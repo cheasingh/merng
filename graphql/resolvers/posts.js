3fa9c934281466a6eed7109c5f656db1a0ec3aa4
Joe Chea <cheasingh168@gmail.com>
2020-07-29 15:01:32 +0700

    add frontend react

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -30,6 +30,11 @@ module.exports = {
   Mutation: {
     async createPost(_, { body }, context) {
       const user = checkAuth(context);
+
+      if (args.body.trim() === "") {
+        throw new Error("Post body must not be empty");
+      }
+
       const newPost = new Post({
         body,
         user: user.ind,
