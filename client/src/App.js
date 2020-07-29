c4d991abb891f7b7736aba4e8630ce715e8e2342
Joe Chea <cheasingh168@gmail.com>
2020-07-29 23:15:00 +0700

    display data from graphql content

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
+import { Container } from "semantic-ui-react";
 import "./App.css";
 
 import MenuBar from "./components/MenuBar";
@@ -10,14 +11,14 @@ import Register from "./pages/Register";
 
 function App() {
   return (
-    <div>
-      <Router>
+    <Router>
+      <Container>
         <MenuBar />
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
-      </Router>
-    </div>
+      </Container>
+    </Router>
   );
 }
 
--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,34 +1,38 @@
 import React, { useState } from "react";
 import { Menu } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 function MenuBar() {
-  const [activeItem, setActiveItem] = useState("");
-
+  // note: this will return /something or /
+  const pathname = window.location.pathname;
+  // note: if / make it home, if not substring it, take everything after /
+  const path = pathname === "/" ? "home" : pathname.substr(1);
+  const [activeItem, setActiveItem] = useState(path);
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
   return (
-    <Menu pointing secondary>
+    <Menu pointing secondary size="massive" color="teal">
       <Menu.Item
         name="home"
         active={activeItem === "home"}
         onClick={handleItemClick}
-      />
-
-      <Menu.Item
-        name="friends"
-        active={activeItem === "friends"}
-        onClick={handleItemClick}
+        as={Link}
+        to="/"
       />
       <Menu.Menu position="right">
         <Menu.Item
           name="login"
           active={activeItem === "login"}
           onClick={handleItemClick}
+          as={Link}
+          to="/login"
         />
         <Menu.Item
           name="register"
           active={activeItem === "register"}
           onClick={handleItemClick}
+          as={Link}
+          to="/register"
         />
       </Menu.Menu>
     </Menu>
