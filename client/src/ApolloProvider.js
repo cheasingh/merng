b718994f887d843ba03ae3f748b7b2ffcb79624c
Joe Chea <cheasingh168@gmail.com>
2020-07-31 23:59:49 +0700

    create post

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -4,6 +4,7 @@ import ApolloClient from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { createHttpLink } from "apollo-link-http";
 import { ApolloProvider } from "@apollo/react-hooks";
+import { setContext } from "apollo-link-context";
 
 // note: initiate apollo connection with backend.
 
@@ -11,8 +12,18 @@ const httplink = createHttpLink({
   uri: "http://localhost:5000",
 });
 
+// note: add authorized token to the header, bearer.
+const authLink = setContext(() => {
+  const token = localStorage.getItem("jwtToken");
+  return {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httplink,
+  link: authLink.concat(httplink),
   cache: new InMemoryCache(),
 });
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useQuery } from "@apollo/react-hooks";
-import gql from "graphql-tag";
 import { Grid } from "semantic-ui-react";
+
+import { AuthContext } from "../context/auth";
 import PostCard from "../components/PostCard";
+import PostForm from "../components/PostForm";
+import { FETCH_POSTS_QUERY } from "../util/graphql";
 
 function Home() {
+  const { user } = useContext(AuthContext);
+
   // note: without set default value before destructuring, it will return undefined, { getPosts: posts } = {} } set this default value to nothing.
   const { loading, data: { getPosts: posts } = {} } = useQuery(
     FETCH_POSTS_QUERY
@@ -16,6 +21,12 @@ function Home() {
         <h1>Recent Posts</h1>
       </Grid.Row>
       <Grid.Row>
+        {user && (
+          <Grid.Column>
+            <PostForm />
+          </Grid.Column>
+        )}
+
         {loading ? (
           <h1>Loading posts...</h1>
         ) : (
@@ -30,27 +41,5 @@ function Home() {
     </Grid>
   );
 }
-
-const FETCH_POSTS_QUERY = gql`
-  {
-    getPosts {
-      id
-      body
-      createdAt
-      username
-      likeCount
-      likes {
-        username
-      }
-      commentCount
-      comments {
-        id
-        username
-        createdAt
-        body
-      }
-    }
-  }
-`;
 
 export default Home;
