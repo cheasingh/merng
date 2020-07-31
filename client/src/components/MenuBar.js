f0589b296f38c9e2cce94ab76f80f7bad14f567a
Joe Chea <cheasingh168@gmail.com>
2020-07-31 21:43:29 +0700

    fix authentication

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Menu } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
+import { AuthContext } from "../context/auth";
+
 function MenuBar() {
+  const { user, logout } = useContext(AuthContext);
   // note: this will return /something or /
   const pathname = window.location.pathname;
   // note: if / make it home, if not substring it, take everything after /
@@ -10,7 +13,14 @@ function MenuBar() {
   const [activeItem, setActiveItem] = useState(path);
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
-  return (
+  const menuBar = user ? (
+    <Menu pointing secondary size="massive" color="teal">
+      <Menu.Item name={user.username} active as={Link} to="/" />
+      <Menu.Menu position="right">
+        <Menu.Item name="logout" onClick={logout} />
+      </Menu.Menu>
+    </Menu>
+  ) : (
     <Menu pointing secondary size="massive" color="teal">
       <Menu.Item
         name="home"
@@ -37,6 +47,8 @@ function MenuBar() {
       </Menu.Menu>
     </Menu>
   );
+
+  return menuBar;
 }
 
 export default MenuBar;
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,83 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { Form, Button } from "semantic-ui-react";
+import { useMutation } from "@apollo/react-hooks";
+import gql from "graphql-tag";
 
-function Login() {
+import { AuthContext } from "../context/auth";
+import { useForm } from "../util/hooks";
+
+function Login(props) {
+  const context = useContext(AuthContext);
+  const [errors, SetErrors] = useState({});
+
+  const { onChange, onSubmit, values } = useForm(loginUserCallBack, {
+    username: "",
+    password: "",
+  });
+
+  const [loginUser, { loading }] = useMutation(LOGIN_USER, {
+    update(_, { data: { login: userData } }) {
+      context.login(userData);
+      props.history.push("/");
+    },
+    onError(err) {
+      SetErrors(err.graphQLErrors[0].extensions.exception.errors);
+    },
+    variables: values,
+  });
+
+  function loginUserCallBack() {
+    loginUser();
+  }
+
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="form-container">
+      <Form onSubmit={onSubmit} noValidate className={loading ? "loading" : ""}>
+        <h1>Login</h1>
+        <Form.Input
+          label="Username"
+          placeholder="Username"
+          type="text"
+          name="username"
+          value={values.username}
+          error={errors.username ? true : false}
+          onChange={onChange}
+        />
+        <Form.Input
+          label="Password"
+          placeholder="Password"
+          type="password"
+          name="password"
+          value={values.password}
+          error={errors.password ? true : false}
+          onChange={onChange}
+        />
+        <Button type="submit" primary>
+          Login
+        </Button>
+      </Form>
+      {Object.keys(errors).length > 0 && (
+        <div className="ui error message">
+          <ui className="list">
+            {Object.values(errors).map((value) => (
+              <li key={value}>{value}</li>
+            ))}
+          </ui>
+        </div>
+      )}
     </div>
   );
 }
+
+const LOGIN_USER = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      id
+      email
+      username
+      token
+    }
+  }
+`;
 
 export default Login;
